6fcb296bc41a3defd9fbddd0367af6e831d2f1f7
nakiwem <nmcdonald@drew.edu>
2024-01-29 13:20:37 -0500

    Update command-injection.py

--- command-injection/command-injection.py
+++ command-injection/command-injection.py
@@ -3,4 +3,7 @@ import os
 filename = input("Please provide a file name to search and display:\n")
 
 command = "cat " + filename
+if filname == os.path.basename(_file_):
+    print("filename cannot be the current running file")
+	os._exit(0)
 os.system(command)
