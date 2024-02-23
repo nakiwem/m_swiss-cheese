3fd97fcb71b958de83f2eaa7b907ad175d05a5f5
nakiwem <157410943+nakiwem@users.noreply.github.com>
2024-02-23 14:24:41 -0500

    Update xss.py

--- xss/xss.py
+++ xss/xss.py
@@ -7,7 +7,7 @@ def index():
     user_input = ''
     if request.method == 'POST':
         # Retrieve user input from the form
-        user_input = request.form.get('user_input', '')
+         user_input = request.form.get('user_input', '').replace('<', '&lt;').replace('>', '&gt;')
 
         # Display user input directly without proper sanitization (for demonstration purposes)
         return render_template('index.html', user_input=user_input, display_script=True)
